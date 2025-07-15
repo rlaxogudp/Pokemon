33be5d5084cf77e3c68ffc3d8a019917ef3961a6
rlaxogudp <140497435+rlaxogudp@users.noreply.github.com>
2025-07-15 15:12:14 +0900

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,25 +1,27 @@
-# Github Action
+# Github Action 
 # 1. py
-# 2. 어떤 OS에서 동작 -> ubuntu
+# 2. 어떤 OS에서 동작?? ubuntu
 # 3. 크롤링 - Selenium
-# 브라우저를 켜지 않고도 할 수 있는가 -> 고민
-# linux 크롬 드라이버
-import selenium.webdriver as wb
+#  - 브라우저를 켜지 않고도 할 수 있는가?
+#  - linux 크롬 드라이버!! 
 
+import selenium.webdriver as wb
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-import csv
-import os
 
 from selenium.webdriver.chrome.options import Options
+
+from selenium.webdriver.chrome.service import Service
+service = Service(executable_path="/usr/bin/chromedriver")
+
 options = Options()
-options.add_argument("--headless") #창없음
+options.add_argument("--headless") # 창 없음
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
 url = "https://pokemonkorea.co.kr/pokedex"
-driver = wb.Chrome()
+driver = wb.Chrome(service=service, options=options)
 driver.maximize_window()
 driver.get(url)
 img = driver.find_element(By.CSS_SELECTOR, "img.img-fluid")
@@ -28,13 +30,18 @@ time.sleep(1)
 h3 = driver.find_element(By.TAG_NAME, "h3")
 name = h3.text.split("\n")[1]
 
-pokemon_exist = os.path.exists("Pokemon.csv")
+driver.close()
+
+import csv
+import os
+pokemon_exist = os.path.exists("pokemon.csv")
 header = ["no", "name"]
 
-with open("Pokemon.csv", "a", newline ="") as file:
+with open("pokemon.csv", "a", newline="") as file:
     writer = csv.writer(file)
 
     if not pokemon_exist:
         writer.writerow(header)
 
     writer.writerow(["0001", name])
+    print("포켓몬 저장 완료!!")
